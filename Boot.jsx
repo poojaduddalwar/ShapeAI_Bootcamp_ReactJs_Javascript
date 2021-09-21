663c6412ef9d301df6e0cbb5f3c8344d7dc86eb9
Pooja Duddalwar <78248594+poojaduddalwar@users.noreply.github.com>
2021-09-21 22:40:31 +0530

    Add files via upload

---
+++ Boot.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Boot() {
+  return (
+    <div className="note">
+      <h1>Elsa</h1>
+      <p>
+        She is introduced as a princess in the fictional Scandinavian Kingdom of
+        Arendelle, heiress to the throne and the elder sister of Princess Anna.
+        Elsa has the magical ability to create and manipulate ice and snow.
+      </p>
+    </div>
+  );
+}
+
+export default Boot;
